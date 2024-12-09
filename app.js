eace4b5b86d8db863f2ff0e363ce35cb7aa9db6b
abdul <taylorkings30@gmail.com>
2024-12-09 11:17:04 +0500

    the code ofr the comments and base is clear moving tot he replay section code

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+
+// select the elements form the dom
+
 const [list_template, main_section, unorder_list] = document.querySelectorAll('#list-template, #Interactive-comments-section, #unorder-list');
 const [reply_templates_section,replay_paragraph,reply_name_tag ] = document.querySelectorAll('#reply-template,  .reply-paragraph, .reply-name');
+const [add_template,add_paragraph,send_button] = document.querySelectorAll('#create-add-comment, .add-paragraph, .add-paragraph');
 
 
+
+// data for the comment section
 const names = {
     name_1: 'John cena',
     name_2: 'Micheal Jordan',
     name_3: 'Lebron James',
     name_4: 'Kobe Bryant',
 }
+
+// ---------------------------------  inside text for the comment section
 
 const inside_texts = {
     text_1: 'hi how are you i heard you were working on a project and i wanted to know how it was going',
@@ -16,12 +24,17 @@ const inside_texts = {
     text_4: 'thanks i will let you know if i need any help you are my best frend abdul',
 }
 
+// ---------------------------------  image source for the comment section
 const img_src = {
     img_1: 'images/avatars/image-amyrobson.png',
     img_2: 'images/avatars/image-juliusomo.png',
     img_3: 'images/avatars/image-maxblagun.png',
     img_4: 'images/avatars/image-ramsesmiron.png',
 }
+
+
+
+// create the comment section
 
 
 function createcommentsection (name, text, imgSrc) {
@@ -38,6 +51,9 @@ function createcommentsection (name, text, imgSrc) {
     return comment_template;
 }
 
+
+// coverting the object to an array and running the loop and calling th fuction to create the comment section
+
 function testthe_template() {
     const nameKeys = Object.keys(names);
     const textKeys = Object.keys(inside_texts);
@@ -50,3 +66,12 @@ function testthe_template() {
 }
 
 testthe_template();
+
+
+
+function add_section() {
+    const add_template_section = add_template.content.cloneNode(true).firstElementChild;
+    unorder_list.appendChild(add_template_section);
+}
+
+add_section();
