6e4fbe94fed9ede5a66079d503bff8ce2a7c9d72
abdul <taylorkings30@gmail.com>
2024-12-12 11:32:15 +0500

    set the data.json to retrieve data

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const [reply_templates_section,replay_paragraph,reply_name_tag ] = document.quer
 const [add_template,add_paragraph,send_button] = document.querySelectorAll('#create-add-comment, .add-paragraph, .add-paragraph');
 
 
-
+/*
 // data for the comment section
 const names = {
     name_1: 'John cena',
@@ -32,19 +32,56 @@ const img_src = {
     img_4: 'images/avatars/image-ramsesmiron.png',
 }
 
+*/
+
+
+
+
+
+
+
+async function getdata() {
+  
+    const response = await fetch('./data.json');
+    if (!response.ok) {
+      throw new Error('Error');
+    }
+    const data = await response.json();
+    return data;
+
+}
+
+getdata().then(data => {
+    if (data) {
+      const { currentUser, comments } = data;
+  
+      // Log the data after it has been fetched
+      console.log('Fetched comments:', comments);
+      console.log('Fetched currentUser:', currentUser);
+     
+      renderComments(comments);
+     
+    }
+    add_section();
+  });
+
+
 
 
 // create the comment section
 
 
-function createcommentsection (name, text, imgSrc) {
+function createcommentsection (comment) {
+    const {user, content, createdAt} = comment;
     const comment_template = list_template.content.cloneNode(true).firstElementChild;
-    const [heading, inside_text] = comment_template.querySelectorAll('.heading, .text-comment');
+    const [heading, time_at, inside_text] = comment_template.querySelectorAll('.heading,.time, .text-comment');
     const img = comment_template.querySelector('.img');
-    if (heading && inside_text && img) {
-        heading.textContent = name;
-        inside_text.textContent = text; 
-        img.src = imgSrc;
+    if (heading && inside_text && img && time_at) {
+        heading.textContent = user.username;
+        inside_text.textContent = content; // Assign content to inside_text
+        time_at.textContent = createdAt; // Assign createdAt to time_at
+        img.src = user.image.png;
+        
     }
     
     
@@ -54,24 +91,29 @@ function createcommentsection (name, text, imgSrc) {
 
 // coverting the object to an array and running the loop and calling th fuction to create the comment section
 
-function testthe_template() {
-    const nameKeys = Object.keys(names);
-    const textKeys = Object.keys(inside_texts);
-    const imgKeys = Object.keys(img_src);
-
-    for (let i = 0; i < nameKeys.length; i++) {
-        const comment_section = createcommentsection(names[nameKeys[i]], inside_texts[textKeys[i]], img_src[imgKeys[i]]);
-        unorder_list.appendChild(comment_section);
+function renderComments(comments) {
+    const commentsContainer = unorder_list;
+    if (comments) {
+        comments.forEach(comment => {
+            const commentSection = createcommentsection(comment);
+            commentsContainer.appendChild(commentSection);
+            if (comment.replies.length > 0) {
+                comment.replies.forEach(reply => {
+                  const reply_section = createcommentsection(reply);
+                  commentsContainer.appendChild(reply_section);
+                });
+              }
+        });
     }
-}
+  
+    }
+  
 
-testthe_template();
 
+    function add_section() {
+        const add_template_section = add_template.content.cloneNode(true).firstElementChild;
+        unorder_list.appendChild(add_template_section);
+    }
+    
+   
 
-
-function add_section() {
-    const add_template_section = add_template.content.cloneNode(true).firstElementChild;
-    unorder_list.appendChild(add_template_section);
-}
-
-add_section();
